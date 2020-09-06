84e7dab6ef898e8584ec4561cb13246a90b4a448
Alvaro <69603031+VaroArt@users.noreply.github.com>
2020-09-06 03:51:56 -0300

    Primeros Scripts
    Se creador los dos primero Scripts para el control del UI
    -Uno que controlara Exclusivamente cosas de la nave
    - y uno segundo que sera solo para crear clases, para cuando existan cosas que se repitan mucho, como los canvas
    
    posiblemente cambiare un poco algunas cosas para tenerlo lo mas ordenado posible (y la verdad si que esta mas ordenado, hasta me da orgullo ver que se puede ocultar todo lo que esta en otro script en el inspector xd)

---
+++ Smolder_rework/Assets/UI V2/Scripts/UI_ControlNaveSc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ControlNaveSc : MonoBehaviour
+{
+    public ControlCanvasNave CanvasNave;
+    public List<ObjControlDeColorNave> ObjsColorNave;
+    public List<ControlDialogos> Dialogos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
---
+++ Smolder_rework/Assets/UI V2/Scripts/UI_ControlSc.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ObjControlDeColorNave
+{
+    public CanvasRenderer ObjetoARenderear;
+    public Color ColorActivo;
+    public Color ColorInactivo;
+}
+
+[System.Serializable]
+public class ControlCanvasNave
+{
+    //public Canvas PanelesNave;
+    public Canvas PanelNaveTexto;
+    public Canvas PanelNaveInteraccion;
+    public Canvas PanelNaveRadar;
+    public Canvas PanelNaveRecarga;
+    public Canvas PanelNavePausa;
+    public Canvas PanelNaveDiario;
+    public Canvas PanelNaveDiarioOff;
+    public Canvas PanelNaveMisiones;
+    public Canvas PanelNavePanelPerros;
+    public Canvas PanelNaveDialogos;
+}
+
+[System.Serializable]
+public class ControlDialogos
+{
+    public bool Dialogos;
+    public int DialogoInteractive;
+}
+
+
+public class UI_ControlSc : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
