35c0b4ab7d000dcf1fea1c2c52a62c19006cb11c
Alvaro <69603031+VaroArt@users.noreply.github.com>
2020-09-13 17:07:40 -0300

    nuevo sistema de interaccion
    este nuevo sistema funciona con un circle collider con un tag de evento o texto que detecta el player y activa uno de los dos botones en la nave

---
+++ Smolder_rework/Assets/UI V2/Scripts/Interaction_Sc.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction_Sc : MonoBehaviour
+{
+    public UI_ControlNaveSc controlNave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Evento Texto"))
+        {
+            controlNave.ControlPanelesNave.ActivarPanelTexto = true;
+            print("allahu akbar");
+        }
+
+        if(other.gameObject.CompareTag("Evento Interaccion"))
+        {
+            controlNave.ControlPanelesNave.ActivarPanelInteraccion = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        controlNave.ControlPanelesNave.ActivarPanelTexto = false;
+        print("rabka uhalla");
+        controlNave.ControlPanelesNave.ActivarPanelInteraccion = false;
+
+    }
+}
