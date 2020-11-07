f9920d1c491c0925aa83f947f84a62a217d0a511
Konahrik1 <69769508+Konahrik1@users.noreply.github.com>
2020-11-06 23:14:05 -0300

    Radar pt. 2
    -Funcionalidad del radar lista
    * se necesitan collider 2d para cada objeto que se quiera que aparesca en el radar, de modo que se generara un ping en este, cuando detecte un collider en su propio layer de minimap.
    * el ping se genera automatico y cambia de color dependiendo del nombre del objeto con el que colisiona. Reaccion en tiempo real, por lo que si se mueve un enemigo por ejm, se vera como parpaeando su ubicacion equisde
    * Existen muchas cosas nuevas que agregar a player, enemigos, props, por lo que un uso completo de la nueva escena de juego 2, es requerido

---
+++ Smolder_rework/Assets/Script/radar/ping_radar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ping_radar : MonoBehaviour
+{
+    [SerializeField]private SpriteRenderer spriterd;
+    [SerializeField] private float disappeartimer;
+    [SerializeField] private float disappeartimerMax;
+    [SerializeField] private Color color;
+
+
+    private void Awake()
+    {
+        spriterd = GetComponent<SpriteRenderer>();
+        disappeartimerMax = 1f;
+        disappeartimer = 0f;
+        color = new Color(1, 1, 1, 1f);
+    }
+    void Start()
+    {
+        
+    }
+
+  
+    void Update()
+    {
+        disappeartimer += Time.deltaTime;
+
+        color.a = Mathf.Lerp(disappeartimerMax, 0f, disappeartimer / disappeartimerMax);
+        spriterd.color = color;
+
+        if(disappeartimer >= disappeartimerMax)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void setColor(Color color)
+    {
+        this.color = color;
+    }
+    public void setdisappeartimer(float disappeartimermax)
+    {
+        this.disappeartimerMax = disappeartimermax;
+        disappeartimer = 0f;
+    }
+}
---
+++ Smolder_rework/Assets/Script/radar/pulse_radar.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pulse_radar : MonoBehaviour
+{
+    [SerializeField] private Transform radarping;
+    public Transform pulsetr;
+    public float range;
+    public float rangeMax;
+    public float rangespeed;
+    public bool activate;
+    [SerializeField] private List<Collider2D> col;
+
+    private void Awake()
+    {
+        col = new List<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (activate)
+        {
+          range += rangespeed * Time.deltaTime;
+        }
+     
+        if (range > rangeMax)
+        {
+            activate = false;
+            range = 0f;
+            col.Clear();  
+        }
+        pulsetr.localScale = new Vector3(range, range);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == ("minimap"))
+        {
+            col.Add(collision);
+            // print(collision.name);
+            foreach (Collider2D colision in col)
+            {
+             Transform radarpingtr =   Instantiate(radarping, collision.gameObject.transform.position, collision.transform.rotation);
+             if (collision.name == ("meteoro minimap"))
+             {
+                    ping_radar radar = radarpingtr.GetComponent<ping_radar>();
+                    radar.setColor(new Color(1, 1, 0));
+
+             }
+             if (collision.name == ("enemy minimap"))
+             {
+                    ping_radar radar2 = radarpingtr.GetComponent<ping_radar>();
+                    radar2.setColor(new Color(1, 0, 0));
+                   //
+             }
+                if (collision.name == ("nave minimap"))
+                {
+                    ping_radar radar2 = radarpingtr.GetComponent<ping_radar>();
+                    radar2.setColor(new Color(0, 1, 0));
+                    //
+                }
+
+            }
+           
+        }
+    
+    }
+   
+    public void pulse()
+    {
+        activate = true;
+    }
+}
