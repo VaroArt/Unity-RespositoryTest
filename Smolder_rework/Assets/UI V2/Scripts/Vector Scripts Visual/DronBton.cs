aa79756b59be22e37d962d709000171d03d8bfc9
Alvaro <69603031+VaroArt@users.noreply.github.com>
2020-11-12 21:17:26 -0300

    UI 3.6
    Se añadio un nuevo icono y se modifico otro para poder animarlo posteriormente

--- Smolder_rework/Assets/UI V2/Scripts/Vector Scripts Visual/DronBton.cs
+++ Smolder_rework/Assets/UI V2/Scripts/Vector Scripts Visual/DronBton.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotatingDeco : MonoBehaviour
+public class DronBton : MonoBehaviour
 {
+
+
     // Start is called before the first frame update
     void Start()
     {
