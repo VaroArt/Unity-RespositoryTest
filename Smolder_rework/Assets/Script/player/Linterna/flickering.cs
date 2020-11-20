8c4158bb98c58a9c546274282a7cfd5b7deead7b
Konahrik1 <69769508+Konahrik1@users.noreply.github.com>
2020-11-20 18:07:08 -0300

    Cambios a luz player
    -Testeos con la luz del player para que parpadee cuando se requiera
    -Eliminado script innecesario de movimiento de camara

---
+++ Smolder_rework/Assets/Script/player/Linterna/flickering.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class flickering : MonoBehaviour
+{
+    public Light2D light2d;
+    [HideInInspector]public float initialTime;
+    public int Min;
+    public int Max;
+    public float delay;
+    [HideInInspector] public int rangeR;
+
+    void Start()
+    {     
+        StartCoroutine(start());
+    }
+
+  
+    void Update()
+    {
+       
+    }
+   IEnumerator start()
+    {
+        yield return new WaitForSeconds(initialTime);
+        StartCoroutine(lightFlicker());
+    }
+    IEnumerator lightFlicker()
+    {
+        yield return new WaitForSeconds(delay);
+        rangeR = Random.Range(Min, Max);
+        if(rangeR == 1)
+        {
+            //print("turn on");
+            light2d.enabled = true;
+        }
+        if(rangeR==2)
+        {
+            // print("turn off");
+            light2d.enabled = false;
+        }
+        StartCoroutine(lightFlicker());
+    }
+}
