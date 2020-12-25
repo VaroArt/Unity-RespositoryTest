05a2dbf94a433a358ee5d49c47c811576f8bc242
Konahrik1 <69769508+Konahrik1@users.noreply.github.com>
2020-12-25 03:17:20 -0300

    Cambios en nebulosas, pruebas parallax
    *Basicamente trate de hacer que funcionara la profundidad, pero no logre nada, lo unico que tiene profundidad son las estrellas. El resto? esta todo encima, aun si se hace pequeño o se aleja mas, sigue sin tener profundidad nada, a esto me refiero con nebulosas por ejm
    *Otro dato que veo, es que si agrego parallax a objetos decorativos, nebulosas etc, podria verse con mas profundidad, ya que se moverian en el mapa de forma distinta PERO el codigo actual para el parallax de estrellas, hace que el objeto se mueva de la posicion inicial mucho, dependiendo de la posicion inicial + la pos de la camara, nose porque, pero las estrellas no se ven afectadas tanto. ENTONCES, 3 dias y no logre nada de eso, ja

--- Smolder_rework/Assets/Script/background/parallax.cs
+++ Smolder_rework/Assets/Script/background/parallax.cs
@@ -4,23 +4,23 @@ using UnityEngine;
 
 public class parallax : MonoBehaviour
 {
-    private float lenght, startpos, startposy;
+   [HideInInspector] public float startpos, ypos;
     public GameObject cam;
     public float parallaxEffect;
-    public float parallaxEffect2;
     void Start()
     {
         startpos = transform.position.x;
-        startposy = transform.position.y;
+        ypos = transform.position.y;
         
-        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+      
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
         float dist = (cam.transform.position.x * parallaxEffect);
         float disty = (cam.transform.position.y * parallaxEffect);
-        transform.position = new Vector3(startpos + dist, startposy + disty, transform.position.z);
+
+        transform.position = new Vector3(startpos + dist, ypos + disty, transform.position.z);
     }
 
 }
