d705e1d889061e1328cb30711f325a7619eb618d
Alvaro <69603031+VaroArt@users.noreply.github.com>
2021-02-03 21:03:36 -0300

    Configuraciones Funcionando
    - Correccion del menu de configuraciones: Ahora este guarda los parametros y los carga, en caso de no haber, usara los que existen por defecto.
    - Cambio Visual en el logo de los creditos
    - Correccion del boton de feedback del menu
    - Añadido un Boton para guardar cambios
    - Añadido un boton de cargar luz, en español

--- Smolder_rework/Assets/UI V2/Scripts/Configuraciones/SettingsScript.cs
+++ Smolder_rework/Assets/UI V2/Scripts/Configuraciones/SettingsScript.cs
@@ -15,6 +15,20 @@ public class SettingsScript : MonoBehaviour
     public AudioMixerGroup BgVolume;
     public AudioMixerGroup FxVolume;
     public AudioMixerGroup MusicVolume;
+    float MasterVolume;
+    float BgVolumen;
+    float FxVolumen;
+    float MusicVolumen;
+
+    public Image SaveIcon;
+    public Animator Saveanim;
+
+    public Toggle FullscreenBool;
+    public Slider MasterVolumeSlider;
+    public Slider BgVolumeSlider;
+    public Slider FxVolumeSlider;
+    public Slider MusicVolumeSlider;
+    public Dropdown QualityDropdown;
 
     public Dropdown resolutionDropdown;
 
@@ -42,8 +56,10 @@ public class SettingsScript : MonoBehaviour
         }
 
         resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
+        //resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+        LoadSettings(currentResolutionIndex);
+        Debug.Log("Actual Resolution: " + Screen.currentResolution);
     }
 
     public void Update()
@@ -55,36 +71,55 @@ public class SettingsScript : MonoBehaviour
     {
         Resolution resolution = resolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen, resolution.refreshRate);
+        Debug.Log("Set Resolution: " + Screen.currentResolution);
     }
 
     public void SetVolume (float volume) //volumen General
     {
         AudioMix.SetFloat("MasterVol", volume);
+        MasterVolume = volume;
+        Debug.Log("Actual Master Volume: " + volume);
     }
 
     public void BgVol (float volume) //volumen BG
     {
         BgVolume.audioMixer.SetFloat("BgVol", volume);
+        BgVolumen = volume;
+        Debug.Log("Actual Bg Volume: " + volume);
     }
 
     public void FxVol (float volume) //Volumen Fx
     {
         FxVolume.audioMixer.SetFloat("FxVol", volume);
+        FxVolumen = volume;
     }
 
     public void MusicVol (float volume) //Volumen Musica
     {
         MusicVolume.audioMixer.SetFloat("MusicVol", volume);
+        MusicVolumen = volume;
     }
 
     public void SetQuality(int qualityIndex) //Calidad Grafica
     {
         QualitySettings.SetQualityLevel(qualityIndex);
+        Debug.Log("Actual Quality: " + qualityIndex);
+        QualityDropdown.value = qualityIndex;
     }
 
     public void SetFullscreen (bool isFullscreen) //Pantalla Completa
     {
         Screen.fullScreen = isFullscreen;
+        Debug.Log("Fullscreen Set: " + isFullscreen);
+
+        if (isFullscreen == true)
+        {
+            FullscreenBool.isOn = true;
+        }
+        if (isFullscreen == false)
+        {
+            FullscreenBool.isOn = false;
+        }
     }
 
     public void ConfigOnOffFunc()
@@ -107,5 +142,102 @@ public class SettingsScript : MonoBehaviour
     public void ButtonConfigOff()
     {
         ConfiguracionesActivas = false;
+    }
+
+    public void SaveAnimation()
+    {
+
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("QualitySettingsPreference", QualityDropdown.value);
+        PlayerPrefs.SetInt("ResolutionPreference", resolutionDropdown.value);
+        PlayerPrefs.SetInt("FullscreenPreference", System.Convert.ToInt32(Screen.fullScreen));
+        PlayerPrefs.SetFloat("MasterVolPreference", MasterVolume);
+        PlayerPrefs.SetFloat("BgVolPreference", BgVolumen);
+        PlayerPrefs.SetFloat("FxVolPreference", FxVolumen);
+        PlayerPrefs.SetFloat("MusicVolPreference", MusicVolumen);
+        Debug.Log("Saved");
+    }
+
+    public void LoadSettings(int currentResolutionIndex)
+    {
+        if (PlayerPrefs.HasKey("QualitySettingsPreference"))
+        {
+            QualityDropdown.value = PlayerPrefs.GetInt("QualitySettingsPreference");
+            Debug.Log("Loadeded Quality: " + QualityDropdown.value);
+        }
+        else
+        {
+            Debug.LogError("Quality Not Set");
+        }
+
+        if (PlayerPrefs.HasKey("ResolutionPreference"))
+        {
+            resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionPreference");
+            Debug.Log("Resolution Loaded: " + Screen.currentResolution);
+        }
+        else
+        {
+            resolutionDropdown.value = currentResolutionIndex;
+            Debug.LogError("Resolution Not Loaded");
+        }
+
+        if (PlayerPrefs.HasKey("FullscreenPreference"))
+        {
+            Screen.fullScreen = System.Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+            FullscreenBool.isOn = System.Convert.ToBoolean(PlayerPrefs.GetInt("FullscreenPreference"));
+            Debug.LogWarning("FullScreen Loaded");
+        }
+        else
+        {
+            Screen.fullScreen = true;
+            Debug.LogError("Defalut Fullscreen");
+        }
+
+        if (PlayerPrefs.HasKey("MasterVolPreference"))
+        {
+            MasterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolPreference");
+            Debug.Log("Loaded Master Volume: " + MasterVolume);
+        }
+        else
+        {
+            MasterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolPreference");
+            Debug.LogError("Error: Master Volume Doesn't load Correctly: " + MasterVolume);
+        }
+
+        if (PlayerPrefs.HasKey("BgVolPreference"))
+        {
+            BgVolumeSlider.value = PlayerPrefs.GetFloat("BgVolPreference");
+            Debug.Log("Load Bg Volume: " + BgVolumen);
+        }
+        else
+        {
+            BgVolumeSlider.value = PlayerPrefs.GetFloat("BgVolPreference");
+            Debug.LogError("Error: Bg Volume Doesn't load Correctly: " + BgVolumen);
+        }
+
+        if (PlayerPrefs.HasKey("FxVolPreference"))
+        {
+            FxVolumeSlider.value = PlayerPrefs.GetFloat("FxVolPreference");
+            Debug.Log("Load Fx Volume: " + FxVolumen);
+        }
+        else
+        {
+            FxVolumeSlider.value = PlayerPrefs.GetFloat("FxVolPreference");
+            Debug.LogError("Error: Fx Volume Doesn't load Correctly: " + FxVolumen);
+        }
+
+        if (PlayerPrefs.HasKey("MusicVolPreference"))
+        {
+            MusicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolPreference");
+            Debug.Log("Load Music Volume: " + MusicVolumen);
+        }
+        else
+        {
+            MusicVolumeSlider.value = PlayerPrefs.GetFloat("MusicVolPreference");
+            Debug.LogError("Error: Music Volume Doesn't load Correctly: " + MusicVolumen);
+        }
     }
 }
