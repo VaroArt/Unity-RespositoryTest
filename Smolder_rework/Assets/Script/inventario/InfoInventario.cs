e1cac0636c2b78e42f1dba01420c3b934adbcd79
Konahrik1 <69769508+Konahrik1@users.noreply.github.com>
2021-02-08 19:45:44 -0300

    Info para inventario
    -Ahora los item del inventario tendran una pequeña info, en este caso al menos, su nombre para saber que es lo que se esta recolectando
    -Cambie el portal en desuso a una carpeta que dice "old". Cuando me asegure que ninguna escena lo esta usando, se elimina del proyecto

---
+++ Smolder_rework/Assets/Script/inventario/InfoInventario.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InfoInventario : MonoBehaviour, IPointerEnterHandler,IPointerExitHandler
+{
+    public GameObject InfoSlot1, InfoSlot2, InfoSlot3;
+    public InventarioSystem inventario;
+    public Text text;
+
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //print(name);
+        if(gameObject.name ==("slot 1"))
+        {
+            print("slot1");
+            InfoSlot1.SetActive(true);
+
+            if(inventario.ItemList[0] == ("Coordenada 1"))
+            {
+                text.text = "Coordenada N°1";
+            }
+            else if(inventario.ItemList[0]==("Coordenada 2"))
+            {
+                text.text = "Coordenada N°2";
+            }
+            else if (inventario.ItemList[0] == ("Llave"))
+            {
+                text.text = "Llave";
+            }
+        }
+        if (gameObject.name == ("slot 2"))
+        {
+            print("slot2");
+            InfoSlot2.SetActive(true);
+
+            if (inventario.ItemList[1] == ("Coordenada 1"))
+            {
+                text.text = "Coordenada N°1";
+            }
+            else if (inventario.ItemList[1] == ("Coordenada 2"))
+            {
+                text.text = "Coordenada N°2";
+            }
+            else if (inventario.ItemList[1] == ("Llave"))
+            {
+                text.text = "Llave";
+            }
+
+        }
+        if (gameObject.name == ("slot 3"))
+        {
+            print("slot3");
+            InfoSlot3.SetActive(true);
+
+            if (inventario.ItemList[2] == ("Coordenada 1"))
+            {
+                text.text = "Coordenada N°1";
+            }
+            else if (inventario.ItemList[2] == ("Coordenada 2"))
+            {
+                text.text = "Coordenada N°2";
+            }
+            else if (inventario.ItemList[2] == ("Llave"))
+            {
+                text.text = "Llave";
+            }
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (gameObject.name == ("slot 1"))
+        {
+            print("slot1");
+            InfoSlot1.SetActive(false);
+        }
+        if (gameObject.name == ("slot 2"))
+        {
+            print("slot2");
+            InfoSlot2.SetActive(false);
+        }
+        if (gameObject.name == ("slot 3"))
+        {
+            print("slot3");
+            InfoSlot3.SetActive(false);
+        }
+    }
+    
+}
